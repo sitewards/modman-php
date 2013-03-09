bbff06c99f4cd944cbb8fde21efdba6f7d35bc50
Tobias Zander <tobias.zander@sitewards.com>
2013-03-09 15:02:45 +0100

    added structure

--- modman.php
+++ modman.php
@@ -1,1 +1,35 @@
 <?php
+
+
+class Modman {
+	public function run($aParameters) {
+		if (!isset($aParameters[1])) {
+			throw new Exception('command not found');
+		}
+
+		switch ($aParameters[1]) {
+			case 'link':
+				break;
+			case 'init':
+				break;
+			default:
+				throw new Exception('command does not exist');
+		}
+
+	}
+}
+
+class Modman_Init {
+
+}
+
+class Modman_Link {
+
+}
+
+class Modman_Status {
+
+}
+
+$oModman = new Modman();
+$oModman->run($argv);
