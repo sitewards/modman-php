6842e67ce87fa4a82acb9b19b2b7e761bb69e029
Aaron Rhodes <arhodes@onepica.com>
2014-01-17 15:21:25 -0500

    Added extra check for folder symlinks when removing

--- modman.php
+++ modman.php
@@ -1057,7 +1057,7 @@ class Modman_Resource_Remover{
 	 */
 	public function doRemoveResource($sElementPath){
 		if (is_dir($sElementPath)){
-			if ($this->isFolderEmpty($sElementPath)){
+			if (is_link($sElementPath) OR $this->isFolderEmpty($sElementPath)){
 				rmdir($sElementPath);
 			}
 		} else if (is_file($sElementPath)){
